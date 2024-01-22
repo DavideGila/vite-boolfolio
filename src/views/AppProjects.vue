f2fd18b5c10b8e5b9c9ad6911f65c353052a4234
DavideGila <davide.gila@gmail.com>
2024-01-22 18:21:08 +0100

    added AppProjects with AppCard imported and made getAllProjects function

---
+++ src/views/AppProjects.vue
@@ -0,0 +1,44 @@
+<template>
+    <h1>Project List</h1>
+    <div class="container">
+        <div class="row">
+            <div v-for="project in projects" :key="project.id" class="col-6">
+                <AppCard :project="project" />
+            </div>          
+        </div>
+    </div>
+    
+    
+    
+</template>
+
+<script>
+import { store } from '../router/store'
+import axios from 'axios';
+import AppCard from '../components/AppCard.vue';
+    export default {
+        name: 'AppProjects',
+        data() {
+            return {
+                store,
+                projects: [],
+            }
+        },
+        methods: {
+            getAllProjects(){
+                axios.get(store.apiUrl + '/projects').then((res) => {
+                    console.log(res.data);
+                    this.projects = res.data.results;
+                })
+            }
+        },
+        components:{ AppCard },
+        mounted() {
+            this.getAllProjects();
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
