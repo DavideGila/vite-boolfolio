430d77db8ae7ec95dde09f0108cfc592e74d6c60
DavideGila <davide.gila@gmail.com>
2024-01-22 18:47:00 +0100

    created navbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/projects">Projects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Projects',
+                    name: 'projects'
+                }
+            ]
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
