88bf3945aafacfd37b2d6d29463c7e8482183e7e
DavideGila <davide.gila@gmail.com>
2024-01-22 18:13:46 +0100

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        <h1>Ciao</h1>
+        <AppHeader />
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-import { store } from './router/store'
+import { store } from './router/store';
+import AppHeader from './components/AppHeader.vue';
 
 export default {
     name: 'App',
@@ -25,6 +27,9 @@ export default {
             })
         },
 
+    },
+    components: {
+        AppHeader
     },
     mounted() {
         this.getAllProjects();
