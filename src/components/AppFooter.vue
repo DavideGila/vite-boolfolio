ab00a862b85f5eb0120b5943945c1e39cab7da6a
DavideGila <davide.gila@gmail.com>
2024-01-22 18:55:36 +0100

    created a simple footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3 class="text-center">Made with &hearts; by Davide Gila</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppFooter'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
