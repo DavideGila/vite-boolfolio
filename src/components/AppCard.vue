77c2ddf6ef41f9ff4d4a9763949096ed1a54fe4b
DavideGila <davide.gila@gmail.com>
2024-01-22 18:22:00 +0100

    created AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <img :src="store.imgPath + project.image" class="card-img-top" :alt="project.title">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.body }}</p>
+            <button><router-link :to="{ name: 'single-project', params: { slug: project.slug } }"></router-link>View More</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { store } from '../router/store'
+export default {
+    name: 'AppCard',
+    props: ['project'],
+    data() {
+        return {
+            store
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
